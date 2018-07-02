2d5083dfd6d4834628846ffec9d7770e3c79b4fe
Med-94 <38832580+Mohamed-94@users.noreply.github.com>
2018-07-03 00:54:28 +0100

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace ImageEditor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.laodI = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.openF = new System.Windows.Forms.OpenFileDialog();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.saveF = new System.Windows.Forms.SaveFileDialog();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(188, 111);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(527, 407);
+            this.panel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(527, 289);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            // 
+            // laodI
+            // 
+            this.laodI.Location = new System.Drawing.Point(12, 82);
+            this.laodI.Name = "laodI";
+            this.laodI.Size = new System.Drawing.Size(75, 23);
+            this.laodI.TabIndex = 1;
+            this.laodI.Text = "Load Image";
+            this.laodI.UseVisualStyleBackColor = true;
+            this.laodI.Click += new System.EventHandler(this.laodI_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(12, 111);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(150, 407);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // openF
+            // 
+            this.openF.FileName = "openFileDialog1";
+            this.openF.Multiselect = true;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth32Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(64, 64);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(217, 56);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Rotate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(206, 13);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Flipe";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(297, 13);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Clone";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(390, 13);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Filter";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(483, 13);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Invert";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // saveF
+            // 
+            this.saveF.FileName = "savF";
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(576, 13);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "Save";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(658, 6);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(83, 34);
+            this.button7.TabIndex = 1;
+            this.button7.Text = "Save As Icon";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(316, 56);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 23);
+            this.button8.TabIndex = 9;
+            this.button8.Text = "Delete";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(518, 56);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(75, 23);
+            this.button9.TabIndex = 10;
+            this.button9.Text = "Zoom Out";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(417, 56);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(75, 23);
+            this.button10.TabIndex = 11;
+            this.button10.Text = "Zoom In";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(666, 56);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(75, 23);
+            this.button11.TabIndex = 12;
+            this.button11.Text = "About";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(758, 539);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.laodI);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Image Editor";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button laodI;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.OpenFileDialog openF;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.SaveFileDialog saveF;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImageEditor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void laodI_Click(object sender, EventArgs e)
+        {
+            string file_name="";
+            listView1.Clear();
+            imageList1.Images.Clear();
+            openF.InitialDirectory = "C:";
+            openF.Title = "Add Image";
+            openF.Filter = "JPEG|*.jpg|PNG|*.png|GIF|*.gif|ALL Images|*.*";
+            openF.FileName = file_name;
+            openF.ShowDialog();
+            if (DialogResult == DialogResult.Cancel) { return; }
+            try
+            {
+                int num_of_files = openF.FileNames.Length;
+                string[] arryfiles = new string[num_of_files ];
+                int counter = 0;
+                foreach (string singl_file in openF.FileNames)
+                {
+                    arryfiles[counter] = singl_file;
+                    imageList1.Images.Add(Image.FromFile(singl_file));
+                    
+                    counter++;
+                }
+                    listView1.LargeImageList = imageList1;
+                    for (int i = 0; i < counter; i++)
+                    {
+                        listView1.Items.Add(arryfiles[i], i);
+                    }
+                
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.Message);
+            }
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string big_filename;
+            for (int i = 0; i < listView1.SelectedItems.Count; i++)
+            {
+                big_filename = listView1.SelectedItems[i].Text;
+                pictureBox1.Image = Image.FromFile(big_filename);
+                panel1.AutoScrollMinSize = new Size(pictureBox1.Image.Width, pictureBox1.Image.Height);
+            }
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image != null)
+            {
+                Bitmap btm = new Bitmap(pictureBox1.Image);
+                btm.RotateFlip(RotateFlipType.Rotate90FlipNone);
+                pictureBox1.Image = btm;
+            }
+            else
+            {
+                MessageBox.Show("No Image");
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image != null)
+            {
+                Bitmap btm = new Bitmap(pictureBox1.Image);
+                btm.RotateFlip(RotateFlipType.RotateNoneFlipX);
+                pictureBox1.Image = btm;
+            }
+            else
+            {
+                MessageBox.Show("No Image");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image != null)
+            {
+                Bitmap bitm = new Bitmap(pictureBox1.Image);
+                Rectangle rec = new Rectangle(0, 0, 100, 100);
+                Bitmap cloneIm = bitm.Clone(rec, System.Drawing.Imaging.PixelFormat.DontCare);
+                pictureBox1.Image = cloneIm;
+            }
+            else
+            {
+                MessageBox.Show("No Such Image!");
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image != null)
+            {
+                Bitmap bim = new Bitmap(pictureBox1.Image);
+                int x, y;
+                for (x = 0; x < bim.Width; x++)
+                {
+                    for (y = 0; y < bim.Height; y++)
+                    {
+                        Color old_color = bim.GetPixel(x, y);
+                        Color new_color = Color.FromArgb(old_color.R, 0, 0);
+                        bim.SetPixel(x, y, new_color);
+
+                    }
+                }
+                pictureBox1.Image = bim;
+            }
+            else
+            {
+                MessageBox.Show("No Such Image");
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image != null)
+            {
+                Bitmap bim = new Bitmap(pictureBox1.Image);
+                int x, y;
+                for (x = 0; x < bim.Width; x++)
+                {
+                    for (y = 0; y < bim.Height; y++)
+                    {
+                        Color old_color = bim.GetPixel(x, y);
+                        Color new_color = Color.FromArgb(255-old_color.R,255-old_color.G  ,255-old_color.B  );
+                        bim.SetPixel(x, y, new_color);
+
+                    }
+                }
+                pictureBox1.Image = bim;
+            }
+            else
+            {
+                MessageBox.Show("No Such Image");
+            }
+        }
+        
+        private void button6_Click(object sender, EventArgs e)
+        {
+            
+            string  savepath = "";           
+            saveF.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+              
+            saveF.Title = "Save As..";
+            saveF.FileName = "Untiteled";
+            saveF.Filter = "JPEG|*.jpg";
+            if (saveF.ShowDialog() != DialogResult.Cancel)
+            {
+                savepath = saveF.FileName;
+                Bitmap bitm = new Bitmap(pictureBox1.Image);
+                bitm.Save(savepath, System.Drawing.Imaging.ImageFormat.Jpeg);
+                MessageBox.Show("Image Saved!");
+            }
+
+        }
+        private void button7_Click(object sender, EventArgs e)
+        {
+            string savepath = "";
+
+            saveF.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+            saveF.Title = "Save As..";
+            saveF.FileName = "Untiteled";
+            saveF.Filter = "Icon|*.ico";
+            if (saveF.ShowDialog() != DialogResult.Cancel)
+            {
+                savepath = saveF.FileName;
+                Icon ico = new System.Drawing.Icon(savepath );
+                Bitmap bitm = new Bitmap(pictureBox1.Image);
+                bitm.Save(savepath, System.Drawing.Imaging.ImageFormat.Icon);
+                MessageBox.Show("Image Saved!");
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            for(int i=0;i<listView1 .SelectedItems.Count ;i++)
+            if(listView1 .SelectedItems[i]!=null)
+            {
+                    listView1.SelectedItems[i].Remove();
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            Bitmap bitm = new Bitmap(pictureBox1.Image);
+            Bitmap bitmnew = new Bitmap(Convert.ToInt32(pictureBox1.Image.Width / 2), Convert.ToInt32(pictureBox1.Image.Height / 2));
+            Graphics graf = Graphics.FromImage(bitmnew);
+            graf.DrawImage(bitm, 0, 0, bitmnew.Width, bitmnew.Height);
+            pictureBox1.Image = bitmnew;
+            panel1.AutoScrollMinSize = new Size(pictureBox1.Image.Width, pictureBox1.Image.Height);
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            Bitmap bitm = new Bitmap(pictureBox1.Image);
+            Bitmap bitmnew = new Bitmap(Convert.ToInt32(pictureBox1.Image.Width * 2), Convert.ToInt32(pictureBox1.Image.Height * 2));
+            Graphics graf = Graphics.FromImage(bitmnew);
+            graf.DrawImage(bitm, 0, 0, bitmnew.Width, bitmnew.Height);
+            pictureBox1.Image = bitmnew;
+            panel1.AutoScrollMinSize = new Size(pictureBox1.Image.Width, pictureBox1.Image.Height);
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            new AboutBox1().ShowDialog();
+            
+        }
+    }
+}
